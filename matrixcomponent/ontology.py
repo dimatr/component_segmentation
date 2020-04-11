242b9faa091ad1e9a1bfdd3d34cbe4d378359bc0
Dmytro Trybushnyi <dima_tr@yahoo.com>
2020-04-11 13:56:17 +0200

    more rdf generator code. Should be more understandable what has come from where

--- matrixcomponent/ontology.py
+++ matrixcomponent/ontology.py
@@ -4,6 +4,24 @@ from rdflib import Namespace, Graph, Literal
 
 class Path:
     id: str
+    ns: Namespace
+
+    def __init__(self):
+        ns = Namespace("") # should be empty!
+
+    def __str__(self):
+        return "path" + str(self.id)  # path1
+
+    def ns_term(self):
+        return self.ns.term(self.__str__())
+
+    def add_to_graph(self, graph: Graph, vg: Namespace) -> None:
+        inner_ns = self.ns.term(self)  # str representation
+
+        # add the object itself
+        graph.add((self.ns, vg.Path, Literal(inner_ns))) # todo: check if indeed vg:Path
+
+        # add its properties, recursively if needed
 
 
 class Node:
@@ -22,15 +40,30 @@ class Node:
         self.linksReverseToReverse = []
 
 
-class Position:
-    id: str
-    cardinality: int
-
-
 class Region:
-    id: str
-    begin: Position
-    end: Position
+    ns: Namespace
+    begin: int
+    end: int
+
+    def __init__(self):
+        ns = Namespace("") # should be path1
+
+    def __str__(self):
+        return "region/" + str(self.begin) + "-" + str(self.end)  # region/2-10
+
+    def ns_term(self):
+        return self.ns.term(self.__str__()) # path1/...
+
+    def add_to_graph(self, graph: Graph, vg: Namespace) -> None:
+        inner_ns = self.ns_term()  # str representation
+
+        # add the object itself
+        graph.add((self.ns, "faldo:Region", Literal(inner_ns)))
+
+        # add its properties, recursively if needed
+        position = self.ns.term("position")
+        graph.add((inner_ns, "faldo:begin", Literal(position.term(str(self.begin)))))
+        graph.add((inner_ns, "faldo:end", Literal(position.term(str(self.end)))))
 
 
 class Step:
@@ -65,6 +98,19 @@ class Row:
     def ns_term(self):
         return self.ns.term(self.__str__())
 
+    def add_to_graph(self, graph: Graph, vg: Namespace) -> None:
+        inner_ns = self.ns.term(self)  # str representation
+
+        # add the object itself
+        graph.add((inner_ns, vg.Row, Literal(self.id)))
+
+        # add its properties, recursively if needed
+        graph.add((inner_ns, vg.positionPercent, Literal(self.positionPercent)))
+        graph.add((inner_ns, vg.inversionPercent, Literal(self.inversionPercent)))
+        for region in self.rowRegion:
+            region.ns = inner_ns
+            region.add_to_graph(graph, vg)
+
 
 class Bin:
     ns: Namespace
@@ -73,7 +119,7 @@ class Bin:
     binRegion: List[Region]
 
     def __init__(self):
-        self.binEdge = []
+        self.binEdge = None
         self.binRegion = []
 
     def __str__(self):
@@ -84,38 +130,42 @@ class Bin:
 
     def add_to_graph(self, graph: Graph, vg: Namespace) -> None:
         inner_ns = self.ns.term(self)  # str representation
-        graph.add((inner_ns, vg.binRank, Literal(self.binRank)))
 
+        # add the object itself
+        graph.add((self.ns, vg.Bin, Literal(self)))
+
+        # add its properties, recursively if needed
+        graph.add((inner_ns, vg.binRank, Literal(self.binRank)))
         for region in self.binRegion:
-            region.ns = inner_ns
+            # !! region.ns is expected to be set when parsing the Path loop
+            graph.add((inner_ns, vg.binRegion, Literal(region.ns_term())))  # can have multiple bins
             region.add_to_graph(graph, vg)
 
-        edge = self.binEdge
-        if edge is not None:
-            graph.add((inner_ns, vg.binEdge, Literal(edge.__str__())))
+        # add the reference to another object if needed
+        if self.binEdge is not None:
+            graph.add((inner_ns, vg.binEdge, Literal(self.binEdge))) # __str__ will be called
 
 
 class Link:
     id: str
-    arrival: List[Bin]
-    departure: List[Bin]
+    arrival: Bin
+    departure: Bin
     paths: List[Path]
 
     def __init__(self):
-        self.arrival = []
-        self.departure = []
         self.paths = []
 
 
 class Component:
     id: str
     ns: Namespace
-    componentEdge: List # List[Component]
+    componentEdge: object # Component
     componentRank: int
     bins: List[Bin]
     rows: List[Row]
 
     def __init__(self):
+        self.componentEdge = None
         self.bins = []
         self.rows = []
 
@@ -127,17 +177,25 @@ class Component:
 
     def add_to_graph(self, graph: Graph, vg: Namespace) -> None:
         inner_ns = self.ns.term(self) # str representation
-        graph.add((inner_ns, vg.componentRank, Literal(self.id)))
 
+        # add the object itself
+        graph.add((self.ns, vg.Component, Literal(self)))
+
+        # add its properties, recursively if needed
+        graph.add((inner_ns, vg.componentRank, Literal(self.id)))
         for bin in self.bins:
             bin.ns = inner_ns
-            graph.add((inner_ns, vg.bins, Literal(bin.ns_term())))
-            bin.add_to_graph(graph, vg)
+            graph.add((inner_ns, vg.bins, Literal(bin.ns_term()))) # can have multiple bins
+            bin.add_to_graph(graph, vg) # add the inner content of each
 
         for i,row in self.rows:
             row.ns = inner_ns
-            graph.add((inner_ns, vg.bins, Literal(row.ns_term())))
-            row.add_to_graph(graph, vg)
+            graph.add((inner_ns, vg.bins, Literal(row.ns_term()))) # can have multiple rows
+            row.add_to_graph(graph, vg) # add the inner content of each
+
+        # add the reference to another object if needed
+        if self.componentEdge is not None:
+            graph.add((inner_ns, vg.componentEdge, Literal(self.componentEdge))) # __str__ will be called
 
 
 class ZoomLevel:
@@ -157,7 +215,9 @@ class ZoomLevel:
 
     def add_to_graph(self, graph: Graph, vg: Namespace) -> None:
         inner_ns = self.ns_term() # str representation
-        graph.add((self.ns, vg.zoomLevel, Literal(self))) # add the object itself
+
+        # add the object itself
+        graph.add((self.ns, vg.ZoomLevel, Literal(self)))
 
         # add its properties, recursively if needed
         graph.add((inner_ns, vg.zoomFactor, Literal(self.zoomFactor)))
